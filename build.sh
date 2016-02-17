052d45675aebf1eb0721a0c7cbb9f5aa8028b56a
root <root@pibox.my.domain>
2016-02-17 01:31:50 +0100

    build.sh update and also commit rootfs.tar.gz

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/ash
 apk upgrade && apk update && apk add bash tzdata
 git clone https://github.com/gliderlabs/docker-alpine
-/docker-alpine/builder/scripts/mkimage-alpine.bash
+./docker-alpine/builder/scripts/mkimage-alpine.bash
 docker build -t privateport/alpine-rpi:3.3 .
 apk del bash tzdata
 rm -rf ./docker-alpine
